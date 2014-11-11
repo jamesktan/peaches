2c2fc71a91107a50ab8ca5117370d852f6c365b7
James Tan <jamesktan@gmail.com>
2014-11-11 17:46:24 +0200

    experiments with calling and special chars

--- Peaches/Peaches/ViewController.swift
+++ Peaches/Peaches/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var test: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dial(sender: AnyObject) {
+        
+        UIApplication.sharedApplication().openURL(NSURL(string: "tel:3000,734224")!)
+    }
 
 }
 
