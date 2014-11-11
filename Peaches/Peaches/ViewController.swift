5032a32a39be1bc9fd2e8fe85e712d4e55d91a3a
James Tan <jamesktan@gmail.com>
2014-11-11 18:27:28 +0200

    some copy pasta that lets us read the address book

--- Peaches/Peaches/ViewController.swift
+++ Peaches/Peaches/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AddressBook
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getAddressBookNames()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +27,106 @@ class ViewController: UIViewController {
     @IBAction func dial(sender: AnyObject) {
         
         UIApplication.sharedApplication().openURL(NSURL(string: "tel:3000,734224")!)
+    }
+
+    /**
+    getAddressBookNames
+    */
+    func getAddressBookNames() {
+        let authorizationStatus = ABAddressBookGetAuthorizationStatus()
+        if (authorizationStatus == ABAuthorizationStatus.NotDetermined)
+        {
+            NSLog("requesting access...")
+            var emptyDictionary: CFDictionaryRef?
+            var addressBook = !(ABAddressBookCreateWithOptions(emptyDictionary, nil) != nil)
+            ABAddressBookRequestAccessWithCompletion(addressBook,{success, error in
+                if success {
+                    self.processContactNames();
+                }
+                else {
+                    NSLog("unable to request access")
+                }
+            })
+        }
+        else if (authorizationStatus == ABAuthorizationStatus.Denied || authorizationStatus == ABAuthorizationStatus.Restricted) {
+            NSLog("access denied")
+        }
+        else if (authorizationStatus == ABAuthorizationStatus.Authorized) {
+            NSLog("access granted")
+            processContactNames()
+        }
+    }
+    
+    func processContactNames()
+    {
+        var errorRef: Unmanaged<CFError>?
+        var addressBook: ABAddressBookRef? = extractABAddressBookRef(ABAddressBookCreateWithOptions(nil, &errorRef))
+        
+        var contactList: NSArray = ABAddressBookCopyArrayOfAllPeople(addressBook).takeRetainedValue()
+        println("records in the array \(contactList.count)")
+        
+        for record:ABRecordRef in contactList {
+            processAddressbookRecord(record)
+            NSLog("--------")
+        }
+    }
+    
+    func processAddressbookRecord(addressBookRecord: ABRecordRef) {
+        var contactName: String = ABRecordCopyCompositeName(addressBookRecord).takeRetainedValue() as NSString
+        NSLog("contactName: \(contactName)")
+        processEmail(addressBookRecord)
+        processPhone(addressBookRecord)
+    }
+    
+    func processEmail(addressBookRecord: ABRecordRef) {
+        let emailArray:ABMultiValueRef = extractABEmailRef(ABRecordCopyValue(addressBookRecord, kABPersonEmailProperty))!
+        for (var j = 0; j < ABMultiValueGetCount(emailArray); ++j) {
+            var emailAdd = ABMultiValueCopyValueAtIndex(emailArray, j)
+            var myString = extractABEmailAddress(emailAdd)
+            NSLog("email: \(myString)")
+        }
+    }
+    
+    func processPhone(addressBookRecord:ABRecordRef) {
+        let phoneArray:ABMultiValueRef = extractABEmailRef(ABRecordCopyValue(addressBookRecord, kABPersonPhoneProperty))!
+        for (var j = 0; j < ABMultiValueGetCount(phoneArray); ++j) {
+            var phoneAdd = ABMultiValueCopyValueAtIndex(phoneArray, j)
+            var myString = extractABPhoneNumber(phoneAdd)
+            NSLog("phone: \(myString)")
+        }
+    }
+    
+    func extractABAddressBookRef(abRef: Unmanaged<ABAddressBookRef>!) -> ABAddressBookRef? {
+        if let ab = abRef {
+            return Unmanaged<NSObject>.fromOpaque(ab.toOpaque()).takeUnretainedValue()
+        }
+        return nil
+    }
+    
+    func extractABEmailRef (abEmailRef: Unmanaged<ABMultiValueRef>!) -> ABMultiValueRef? {
+        if let ab = abEmailRef {
+            return Unmanaged<NSObject>.fromOpaque(ab.toOpaque()).takeUnretainedValue()
+        }
+        return nil
+    }
+    func extractABPhoneRef (abPhoneRef: Unmanaged<ABMultiValueRef>!) -> ABMultiValueRef? {
+        if let ab = abPhoneRef {
+            return Unmanaged<NSObject>.fromOpaque(ab.toOpaque()).takeUnretainedValue()
+        }
+        return nil
+    }
+    
+    func extractABEmailAddress (abEmailAddress: Unmanaged<AnyObject>!) -> String? {
+        if let ab = abEmailAddress {
+            return Unmanaged.fromOpaque(abEmailAddress.toOpaque()).takeUnretainedValue() as CFStringRef
+        }
+        return nil
+    }
+    func extractABPhoneNumber(abPhoneNumber: Unmanaged<AnyObject>!) -> String? {
+        if let ab = abPhoneNumber {
+            return Unmanaged.fromOpaque(abPhoneNumber.toOpaque()).takeUnretainedValue() as CFStringRef
+        }
+        return nil
     }
 
 }
