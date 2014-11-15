363b53cf1a736bfc9613fc260ad0e9b3a4d2598d
James Tan <jamesktan@gmail.com>
2014-11-15 16:22:40 +0200

    added peach interface

---
+++ Peaches/PeachInterface/PeachInterface.h
@@ -0,0 +1,19 @@
+//
+//  PeachInterface.h
+//  PeachInterface
+//
+//  Created by James Tan on 11/15/14.
+//  Copyright (c) 2014 Axon Flux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PeachInterface.
+FOUNDATION_EXPORT double PeachInterfaceVersionNumber;
+
+//! Project version string for PeachInterface.
+FOUNDATION_EXPORT const unsigned char PeachInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PeachInterface/PublicHeader.h>
+
+
